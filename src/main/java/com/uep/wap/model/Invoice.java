3c355acb363ebb8224dd22456da574446b6bfd6c
ryba24 <bartekrybarczyk2424@gmail.com>
2026-03-24 18:53:30 +0100

    dokonczona klasa invoice

--- src/main/java/com/uep/wap/model/Invoice.java
+++ src/main/java/com/uep/wap/model/Invoice.java
@@ -9,11 +9,26 @@ public class Invoice {
     @Column(name ="id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)//
     private long id;
+    @Column(name ="invoiceAmount")
+    private long invoiceAmount;
     @Column(name ="invoiceNumber")
     private String invoiceNumber;
-    @Column(name ="points")
-    private Integer points;
-    @Column(name ="name")
+    @Column(name ="issueDate")
+    private String issueDate;
+    @Column(name ="PaymentStatus")
+    private String PaymentStatus;
+
+    public Invoice(){
+
+    }
+
+    public Invoice(long id, long invoiceAmount, String invoiceNumber, String issueDate, String paymentStatus) {
+        this.id = id;
+        this.invoiceAmount = invoiceAmount;
+        this.invoiceNumber = invoiceNumber;
+        this.issueDate = issueDate;
+        PaymentStatus = paymentStatus;
+    }
 
     public void setId(long id){
         this.id = id;
@@ -21,8 +36,29 @@ public class Invoice {
     public long getId(){
         return id;
     }
-    public Invoice(){
 
+    public long getInvoiceAmount() {
+        return invoiceAmount;
+    }
+
+    public void setInvoiceAmount(long invoiceAmount) {
+        this.invoiceAmount = invoiceAmount;
+    }
+
+    public String getIssueDate() {
+        return issueDate;
+    }
+
+    public void setIssueDate(String issueDate) {
+        this.issueDate = issueDate;
+    }
+
+    public String getPaymentStatus() {
+        return PaymentStatus;
+    }
+
+    public void setPaymentStatus(String paymentStatus) {
+        PaymentStatus = paymentStatus;
     }
 
     public String getInvoiceNumber() {
@@ -33,17 +69,6 @@ public class Invoice {
         this.invoiceNumber = invoiceNumber;
     }
 
-    public Integer getPoints() {
-        return points;
-    }
-
-    public void setPoints(Integer points) {
-        this.points = points;
-    }
-
-    public Invoice(String invoiceNumber, Integer points){
-
-    }
 }
 
 
