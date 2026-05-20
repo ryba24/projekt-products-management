0f172c8e5614faee1f5ca2a23a724c40fcb3af0f
Ros <ros.pol16@gmail.com>
2026-05-20 16:28:17 +0200

    added cors

--- src/main/java/com/uep/wap/repository/RoleTypeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.uep.wap.repository;
-
-import com.uep.wap.model.RoleType;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RoleTypeRepository extends CrudRepository<RoleType, Integer> {
-}
